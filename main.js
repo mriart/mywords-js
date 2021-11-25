4bd98fead30995a829a73b45a23f8345ce59c6db
mriart <marc_riart@es.ibm.com>
2021-11-25 19:48:31 +0100

    Incorporated mywords.txt

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var app = express();
 var port = 8080;
+var fs = require('fs');
+var myWords = [];
+var myCard = [];
+var debug = 1;
+
+//Return random card of mywords.txt, that is a line in array format
+function getRandomCard() {
+	var idx = Math.floor(Math.random() * myWordsLength);
+	myCard = myWords[idx].split(":");
+	return myCard;
+}
+
+//Initialize
+myWords = fs.readFileSync('./mywords.txt').toString().split('\n');
+myWords.pop();
+myWordsLength = myWords.length;
+if (debug == 1) console.log('initialization:', myWords, myWordsLength);
 
 app.set("view engine", "ejs");
 app.get ("/", function (req,res) {
-	res.render("answer.ejs", {quiz: "gosar", quizExample: "Com goses dir això", translation: "dare", sound: "dee", example: "How do you dare?"});	
+	myCard = getRandomCard();
+	if (debug == 1) console.log('getRandomCard:', myCard);
+	res.render("answer.ejs", {quiz: myCard[0], quizExample: myCard[1], translation: myCard[2], sound: myCard[3], example: myCard[4]});	
 });
 
 app.listen(port, function(err){
